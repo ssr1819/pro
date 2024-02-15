4471a7b0c49bb80accbd6739cfabf5e4c62ec89d
ssr1819 <149999089+ssr1819@users.noreply.github.com>
2024-02-15 10:48:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,29 @@
 provider "aws" {
   region = "ap-south-1"
 }
+
+# Launch an EC2 instance
+resource "aws_instance" "my_instance" {
+  ami             = "ami-0d3f444bc76de0a79"  # Replace with the desired AMI ID
+  instance_type   = "t2.micro"  # Change this to the desired instance type
+  subnet_id       = aws_subnet.my_subnet.id
+ 
+
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
+
+
+
+ user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              service httpd start
+              chkconfig httpd on
+              firewall-cmd --zone=public --add-port=80/tcp --permanent
+              firewall-cmd --reload
+              EOF
+
+  tags = {
+    Name = "MyInstance"
+  }
+}
