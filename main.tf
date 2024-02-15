2f28b429b43874eae62e24d38c83817b528b9d78
ssr1819 <149999089+ssr1819@users.noreply.github.com>
2024-02-15 11:59:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,14 @@
 provider "aws" {
   region = "ap-south-1"
+  Access_key = "AKIA4WD377HHR6ETJWPB"
+  Secret_key = "WkNc0O49FU8aWQpRU/SUkszzpk7gqgs2bSkb5swE"
 }
-
 # Launch an EC2 instance
 resource "aws_instance" "my_instance" {
-  ami             = "ami-0d3f444bc76de0a79"  # Replace with the desired AMI ID
+  ami             = "ami-0449c34f967dbf18a"  # Replace with the desired AMI ID
   instance_type   = "t2.medium"  # Change this to the desired instance type
-  
+  count = 1
+
   tags = {
     Name = "MyInstance"
   }
